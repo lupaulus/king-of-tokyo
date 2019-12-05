d9dc6a3ffc87d102ad3575245800b1c8d9eda484
Lucas PAULUS <lucas67500@gmail.com>
2019-12-05 23:09:57 +0100

    Avancement du code du modele

--- KingOfTokyo/ServeurKoT/Modele/Monstre.cs
+++ KingOfTokyo/ServeurKoT/Modele/Monstre.cs
@@ -8,31 +8,64 @@ using System.Text;
 namespace ServeurKoT.Modele{
     public class Monstre {
 
-        public Monstre() {
+        public Monstre(string nomDonne) {
+            nom = nomDonne;
+            pointVictoire = POINT_VICTOIRE_BASE;
+            pointVie = POINT_VIE_BASE;
+            energie = POINT_ENERGIE_BASE;
         }
 
-        private string nom;
+        public string nom { get; }
+        public int pointVie { get; set; } 
+        public int pointVictoire { get; set; }
+        public int energie { get; set; }
 
-        private int pointVie;
+        private static int POINT_VIE_MIN = 0;
 
-        private int pointVictoire;
+        private static int POINT_VICTOIRE_BASE = 0;
 
-        private int energie;
+        private static int POINT_VIE_MAX = 12;
 
         private static int POINT_ENERGIE_MAX = 20;
 
         private static int POINT_ENERGIE_BASE = 0;
 
         private static int POINT_VIE_BASE = 10;
+        private List<Carte> listeCartes { get; }
+        private List<Carte> effetCartes { get; }
 
-        private HashSet<Carte> effetCartes;
-
-
-
-
-
-
-
+        /// <summary>
+        /// Méthode pour rétirer la vie hors combat
+        /// </summary>
+        /// <param name="value">Valeur de vie à ajouter</param>
+        public void retirerVie(int value)
+        {
+            pointVie -= value;
+            if(pointVie < POINT_VIE_MIN)
+            {
+                pointVie = POINT_VIE_MIN;
+            }
+        }
+        /// <summary>
+        /// Méthode pour ajouter la vie hors combat
+        /// </summary>
+        /// <param name="value">Valeur de vie à retirer</param>
+        public void ajouterVie(int value)
+        {
+            pointVie += value;
+            if(pointVie > POINT_VIE_MAX)
+            {
+                pointVie = POINT_VIE_MAX;
+            }
+        }
+        public void frapper(Monstre monstreAFrapper, int valeurDegat)
+        {
+            monstreAFrapper.seFaireFrapper(valeurDegat);
+        }
+        public void seFaireFrapper(int valeurDegat)
+        {
+            retirerVie(valeurDegat);
+        }
 
         public void appliquerJeton() {
             // TODO implement here
@@ -49,6 +82,7 @@ namespace ServeurKoT.Modele{
         public void achatCarte() {
             // TODO implement here
         }
+
 
     }
 }
--- KingOfTokyo/ServeurKoT/Modele/Plateau.cs
+++ KingOfTokyo/ServeurKoT/Modele/Plateau.cs
@@ -1,21 +1,59 @@
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace ServeurKoT.Modele{
+    /// <summary>
+    /// Singleton
+    /// </summary>
     public class Plateau {
 
-        public Plateau() {
-
+        /// <summary>
+        /// Création du plateau
+        /// </summary>
+        /// <param name="monstres">Liste des monstres qui compose le plateau</param>
+        public Plateau(List<Monstre> monstres) 
+        {
+            tousLesMonstres = new List<Monstre>(monstres);
+            monstreEnVie = new List<Monstre>(monstres);
         }
 
-        private List<Monstre> monstreEnVille;
+        private List<Monstre> monstreEnVille { get; }
 
-        private List<Monstre> monstreEnVie;
+        private List<Monstre> monstreEnVie { get; }
 
-        private List<Monstre> tousLesMonstres;
+        private List<Monstre> tousLesMonstres { get; }
+
+        
+        /// <summary>
+        /// Methode pour rentrer dans la ville
+        /// </summary>
+        /// <param name="monstre">Monstre qui doit rentrer dans la ville</param>
+        public void entrerVille(Monstre monstre)
+        {
+            monstreEnVille.Add(monstre);
+        }
+
+        /// <summary>
+        /// Methode pour sortir de la ville
+        /// </summary>
+        /// <param name="monstre">Monstre qui doit sortir la ville</param>
+        public void sortirVille(Monstre monstre)
+        {
+            monstreEnVille.Remove(monstre);
+        }
+
+        /// <summary>
+        /// Méthode qui ajoute le monstre mort dans la ville
+        /// </summary>
+        /// <param name="monstre"></param>
+        public void monstreMort(Monstre monstre)
+        {
+            monstreEnVie.Remove(monstre);
+        }
 
 
 
