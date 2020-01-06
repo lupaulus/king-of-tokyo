6264041ae3273f29e16bf7723927ce19efe9ddb3
Lucas PAULUS <lucas67500@gmail.com>
2020-01-06 22:07:40 +0100

    Import de cartes

--- KingOfTokyo/ServeurKoT/Controleur/GCarte.cs
+++ KingOfTokyo/ServeurKoT/Controleur/GCarte.cs
@@ -1,5 +1,6 @@
 
 using ServeurKoT.Modele;
+using SimpleLogger;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,17 +19,24 @@ namespace ServeurKoT.Controleur{
 
         private void ImportCartes()
         {
+            Logger.Log(Logger.Level.Info, "Import Cartes en cours");
             ListDesCartes = new Stack<Carte>();
 
             FactoryCarteAction factoryCarteAction = new FactoryCarteAction();
             FactoryCartePouvoir factoryCartePouvoir = new FactoryCartePouvoir();
 
             List<Carte> list = factoryCarteAction.AjouterCarte();
+            int nbrAction = list.Count;
             list.AddRange(factoryCartePouvoir.AjouterCarte());
 
             //C'est sale mamene
-            ListDesCartes = (Stack<Carte>) list.Cast<Carte>();
+            ListDesCartes = new Stack<Carte>(list);
+            Logger.Log(Logger.Level.Info, 
+                String.Format("Import Cartes fini; {0} Cartes au total",list.Count));
+            Logger.Log(Logger.Level.Info,
+                String.Format("{0} Cartes Actions | {1} Cartes Pouvoir", nbrAction,list.Count-nbrAction));
         }
-     
+
+
     }
 }
