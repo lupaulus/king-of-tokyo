26774934ba639e742eca2206849acd32f173b54b
Thomas DURR <thms.durr@gmail.com>
2020-01-06 11:48:19 +0100

    update titre windows/pages + début Menu.xaml.cs

--- KingOfTokyo/ClientKoT/Menu.xaml.cs
+++ KingOfTokyo/ClientKoT/Menu.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,43 @@ namespace ClientKoT
         public Menu()
         {
             InitializeComponent();
+        }
+
+        private void ButtonConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxPseudo.Text.Length == 0)
+            {
+                errormessage.Text = "Entrer un Pseudonyme.";
+                textBoxPseudo.Focus();
+            }
+            else if (textBoxIP.Text.Length == 0)
+            {
+                errormessage.Text = "Entrer une IP.";
+                textBoxIP.Focus();
+            }
+            else if (textBoxPort.Text.Length == 0)
+            {
+                errormessage.Text = "Entrer un Port.";
+                textBoxPort.Focus();
+            }
+            else if (!Regex.IsMatch(textBoxIP.Text, @"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"))
+            {
+                errormessage.Text = "Entrer une adresse IP valide.";
+                textBoxIP.Select(0, textBoxIP.Text.Length);
+                textBoxIP.Focus();
+            }
+            else if (!Regex.IsMatch(textBoxPort.Text, @"^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$"))
+            {
+                errormessage.Text = "Entrer un Port valide.";
+                textBoxPort.Select(0, textBoxPort.Text.Length);
+                textBoxPort.Focus();
+            }
+            else
+            {
+                string pseudo = textBoxPseudo.Text;
+                string ip = textBoxIP.Text;
+                int port = int.Parse(textBoxPort.Text);
+            }
         }
     }
 }
