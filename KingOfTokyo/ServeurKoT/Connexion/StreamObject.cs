4e647f85af5dfa9a78d11d94a1c14e7a4db259e2
Lucas PAULUS <lucas67500@gmail.com>
2020-01-20 14:45:11 +0100

    Modification communication TCP

--- KingOfTokyo/ServeurKoT/Connexion/StreamObject.cs
+++ KingOfTokyo/ServeurKoT/Connexion/StreamObject.cs
@@ -1,7 +1,19 @@
-﻿namespace ServeurKoT.Connexion
+﻿using System;
+
+namespace ServeurKoT.Connexion
 {
     public class StreamObject
     {
         public int Length { get; internal set; }
+
+        internal static StreamObject FromBytes(byte v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal byte IntoBytes()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
