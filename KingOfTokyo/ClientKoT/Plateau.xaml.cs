201b244171e4d79e120de872a40b0fa24f3e3d65
TD <thms.durr@gmail.com>
2020-03-27 13:18:05 +0100

    visibility nbJoueurs, switch case infâme

--- KingOfTokyo/ClientKoT/Plateau.xaml.cs
+++ KingOfTokyo/ClientKoT/Plateau.xaml.cs
@@ -44,5 +44,112 @@ namespace ClientKoT
                 lblPret.Content = "Vous n'êtes pas prêt !";
             }
         }
+
+        private void btnHide_Click(object sender, RoutedEventArgs e)
+        {
+            switch(int.Parse(nbHideJoueur.Text))
+            {
+                case 0: // 0 joueur
+                    j1.Visibility = Visibility.Hidden;
+                    j2.Visibility = Visibility.Hidden;
+                    j3.Visibility = Visibility.Hidden;
+                    j4.Visibility = Visibility.Hidden;
+                    j5.Visibility = Visibility.Hidden;
+                    j6.Visibility = Visibility.Hidden;
+                    borderj1.Visibility = Visibility.Hidden;
+                    borderj2.Visibility = Visibility.Hidden;
+                    borderj3.Visibility = Visibility.Hidden;
+                    borderj4.Visibility = Visibility.Hidden;
+                    borderj5.Visibility = Visibility.Hidden;
+                    borderj6.Visibility = Visibility.Hidden;
+                    break;
+                case 1: // 1 joueur
+                    j1.Visibility = Visibility.Visible;
+                    j2.Visibility = Visibility.Hidden;
+                    j3.Visibility = Visibility.Hidden;
+                    j4.Visibility = Visibility.Hidden;
+                    j5.Visibility = Visibility.Hidden;
+                    j6.Visibility = Visibility.Hidden;
+                    borderj1.Visibility = Visibility.Visible;
+                    borderj2.Visibility = Visibility.Hidden;
+                    borderj3.Visibility = Visibility.Hidden;
+                    borderj4.Visibility = Visibility.Hidden;
+                    borderj5.Visibility = Visibility.Hidden;
+                    borderj6.Visibility = Visibility.Hidden;
+                    break;
+                case 2: // 2 joueurs
+                    j1.Visibility = Visibility.Visible;
+                    j2.Visibility = Visibility.Visible;
+                    j3.Visibility = Visibility.Hidden;
+                    j4.Visibility = Visibility.Hidden;
+                    j5.Visibility = Visibility.Hidden;
+                    j6.Visibility = Visibility.Hidden;
+                    borderj1.Visibility = Visibility.Visible;
+                    borderj2.Visibility = Visibility.Visible;
+                    borderj3.Visibility = Visibility.Hidden;
+                    borderj4.Visibility = Visibility.Hidden;
+                    borderj5.Visibility = Visibility.Hidden;
+                    borderj6.Visibility = Visibility.Hidden;
+                    break;
+                case 3: // 3 joueurs
+                    j1.Visibility = Visibility.Visible;
+                    j2.Visibility = Visibility.Visible;
+                    j3.Visibility = Visibility.Visible;
+                    j4.Visibility = Visibility.Hidden;
+                    j5.Visibility = Visibility.Hidden;
+                    j6.Visibility = Visibility.Hidden;
+                    borderj1.Visibility = Visibility.Visible;
+                    borderj2.Visibility = Visibility.Visible;
+                    borderj3.Visibility = Visibility.Visible;
+                    borderj4.Visibility = Visibility.Hidden;
+                    borderj5.Visibility = Visibility.Hidden;
+                    borderj6.Visibility = Visibility.Hidden;
+                    break;
+                case 4: // 4 joueurs
+                    j1.Visibility = Visibility.Visible;
+                    j2.Visibility = Visibility.Visible;
+                    j3.Visibility = Visibility.Visible;
+                    j4.Visibility = Visibility.Visible;
+                    j5.Visibility = Visibility.Hidden;
+                    j6.Visibility = Visibility.Hidden;
+                    borderj1.Visibility = Visibility.Visible;
+                    borderj2.Visibility = Visibility.Visible;
+                    borderj3.Visibility = Visibility.Visible;
+                    borderj4.Visibility = Visibility.Visible;
+                    borderj5.Visibility = Visibility.Hidden;
+                    borderj6.Visibility = Visibility.Hidden;
+                    break;
+                case 5: // 5 joueurs
+                    j1.Visibility = Visibility.Visible;
+                    j2.Visibility = Visibility.Visible;
+                    j3.Visibility = Visibility.Visible;
+                    j4.Visibility = Visibility.Visible;
+                    j5.Visibility = Visibility.Visible;
+                    j6.Visibility = Visibility.Hidden;
+                    borderj1.Visibility = Visibility.Visible;
+                    borderj2.Visibility = Visibility.Visible;
+                    borderj3.Visibility = Visibility.Visible;
+                    borderj4.Visibility = Visibility.Visible;
+                    borderj5.Visibility = Visibility.Visible;
+                    borderj6.Visibility = Visibility.Hidden;
+                    break;
+                case 6: // 6 joueurs
+                    j1.Visibility = Visibility.Visible;
+                    j2.Visibility = Visibility.Visible;
+                    j3.Visibility = Visibility.Visible;
+                    j4.Visibility = Visibility.Visible;
+                    j5.Visibility = Visibility.Visible;
+                    j6.Visibility = Visibility.Visible;
+                    borderj1.Visibility = Visibility.Visible;
+                    borderj2.Visibility = Visibility.Visible;
+                    borderj3.Visibility = Visibility.Visible;
+                    borderj4.Visibility = Visibility.Visible;
+                    borderj5.Visibility = Visibility.Visible;
+                    borderj6.Visibility = Visibility.Visible;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
