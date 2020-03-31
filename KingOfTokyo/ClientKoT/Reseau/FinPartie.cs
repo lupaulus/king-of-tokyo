7c394ab35098914ca46dbef053c8dd90d287134d
Lucas Paulus <lucas.paulus@lohr.fr>
2020-03-31 08:31:57 +0200

    Paquet FIn de partie

---
+++ KingOfTokyo/ClientKoT/Reseau/FinPartie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Client.Reseau
+{
+
+    public class FinPartie : StreamObject
+    {
+        private string s;
+
+        public FinPartie()
+        {
+        }
+
+        public FinPartie(string s)
+        {
+            this.s = s;
+        }
+
+        public override string IntoString()
+        {
+            return $"";
+        }
+
+       
+    }
+}
---
+++ KingOfTokyo/ServeurKoT/Reseau/FinPartie.cs
@@ -0,0 +1,29 @@
+﻿using ServeurKoT.Controleur;
+using ServeurKoT.Modele;
+using System;
+using System.Collections.Generic;
+
+namespace ServeurKoT.Reseau
+{
+
+    public class FinPartie : StreamObject
+    {
+        private string s;
+
+        public FinPartie()
+        {
+        }
+
+        public FinPartie(string s)
+        {
+            this.s = s;
+        }
+
+        public override string IntoString()
+        {
+            return $"";
+        }
+
+       
+    }
+}
